5a70ea9abc6aad6d74bb28243a6dadf9f7c7ddde
jobas <job.t.developer@gmail.com>
2023-07-02 18:52:55 -0400

    Adjusted landing page call to action section and added new domain to corsOptions

--- backend/config/corsOptions.js
+++ backend/config/corsOptions.js
@@ -1,7 +1,7 @@
 
 
 const corsOptions = {
-  origin: ['https://cadinov.onrender.com', 'http://localhost:3000'],
+  origin: ['https://cadinov.onrender.com', 'https://www.cadinovtours.com', 'http://localhost:3000'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
--- frontend/src/screens/LandingScreen.js
+++ frontend/src/screens/LandingScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useRef, useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
@@ -7,203 +7,124 @@ AOS.init({
   duration: 2000
 })
 
-//function LandingScreen () {
+const LandingScreen = () => {
+  const history = useHistory()
+  const videoRef = useRef() // create a ref for the video element
 
-class LandingScreen extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      handleClick: () => {
-        window.location.href = '/home';
+  useEffect(() => {
+    // set the callback for the 'onLoadedData' event within the useEffect
+    const videoElement = videoRef.current
+    if (videoElement) {
+      videoElement.onloadeddata = () => {
+        videoElement
+          .play()
+          .then(() => {
+            console.log('Video was successfully played')
+          })
+          .catch(error => {
+            console.error(error)
+          })
       }
-    };
+    }
+
+    return () => {
+      // clean up event listener when the component is unmounted
+      if (videoElement) {
+        videoElement.onloadeddata = null
+      }
+    }
+  }, [])
+
+  const handleClick = () => {
+    history.push('/home')
   }
 
-
-  componentDidMount() {
-
-        // Get the video element
-        const videoElement = document.getElementById('my-video');
-      
-        // Check if the video element exists and if it supports the play method
-        if (videoElement && videoElement.play) {
-          // Try to play the video
-          videoElement.play()
-            .then(() => {
-              // Video was successfully played
-
-              // console.log('success!')
-            })
-            .catch(error => {
-              // An error occurred, such as the user clicking on the video to pause it
-              console.error(error);
-            });
-        };
-      }
-  
-  
-  /* forces the page to reload to home screen once
-    button is clicked */
-  // const history = useHistory()
-
-  // const handleClick = () => {
-  //   window.location.href = '/home'
-  // }
-
-  
-render(){
-
+  // add ref to the video element
   return (
     <div className='row landing justify-content-center'>
-        {/* prevents exposed whitespaces around webpage*/}
+      <style>
+        {`
+          body {
+            overflow: hidden;
+          }
+        `}
+      </style>
+      <video
+        ref={videoRef}
+        src='./videos/cadinov-intro.mp4'
+        autoPlay
+        loop
+        muted // this line is new
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '120vw',
+          height: '115vh',
+          zIndex: -1,
+          objectFit: 'cover'
+        }}
+      />
+      <div
+        className='col-xs-12 col-sm-10 col-md-10 my-auto text-center'
+        style={{
+          borderRight: '8px solid white',
+          background:
+            'linear-gradient(to bottom right, rgba(23, 85, 106, 1), rgba(204, 204, 204, 0.3))',
+          height: '50vh',
+          display: 'flex', // new
+          flexDirection: 'column', // new
+          justifyContent: 'center', // new
+          alignItems: 'center', // new
+          flex: 1,
+          position: 'relative', // this line is new
+          top: '-30vh'
+        }}
+      >
         <style>
-            {`
-            body {
-                overflow: hidden;
+          {`
+            .pulse-button {
+              animation: pulse 2s infinite;
             }
-            `}
+            
+            @keyframes pulse {
+              0% {
+                transform: scale(1);
+              }
+              50% {
+                transform: scale(1.05);
+              }
+              100% {
+                transform: scale(1);
+              }
+            }
+          `}
         </style>
-        <video
-            src='./videos/cadinov-intro.mp4'
-            autoPlay
-            loop
-            style={{
-                position: 'absolute',
-                top: 0,
-                left: 0,
-                width: '100vw',
-                height: '100vh',
-                zIndex: -1,
-                objectFit:'cover'
-            }}
+        <img
+          src='./images/cadinov_logo.png'
+          className='pulse-image'
+          alt='Cadinov Logo'
+          data-aos='zoom-in'
+          style={{
+            width: '20vw',
+            height: '20vw'
+          }}
         />
-        <div
-            className='col-md-10 my-auto text-center'
-            style={{
-                borderRight: '8px solid white',
-                background: 'linear-gradient(to bottom right, rgba(23, 85, 106, 1), rgba(204, 204, 204, 0.3))',
-                height: '50vh'
-            }}
+        <h1
+          data-aos='zoom-out'
+          style={{
+            color: 'white',
+            fontSize: '3vw'
+          }}
         >
-            <style>
-                {`
-                .pulse-button {
-                    animation: pulse 2s infinite;
-                }
-                
-                @keyframes pulse {
-                    0% {
-                        transform: scale(1);
-                    }
-                    50% {
-                        transform: scale(1.05);
-                    }
-                    100% {
-                        transform: scale(1);
-                    }
-                }
-                `}
-            </style>
-            <img
-                src='./images/cadinov_logo.png'
-                className='pulse-image'
-                alt='Cadinov Logo'
-                data-aos='zoom-in'
-                style={{
-                    width: '20vw',
-                    height: '20vw'
-                }}
-            />
-            <h1
-                data-aos='zoom-out'
-                style={{
-                    color: 'white',
-                    fontSize: '3vw'
-                }}
-            >
-                "Promoting the development of national and international tourism"
-            </h1>
-            <button
-                className='btn btn-primary pulse-button'
-                onClick={this.state.handleClick}
-            >
-                Begin Your Adventure
-            </button>
-        </div>
+          "Promoting the development of national and international tourism"
+        </h1>
+        <button className='btn btn-primary pulse-button' onClick={handleClick}>
+          Begin Your Adventure
+        </button>
+      </div>
     </div>
-);
-
-
-  //   return (
-
-  //     <div className='row landing justify-content-center'>
-  //         {/* prevents exposed whitespaces around webpage*/}
-  //         <style>
-  //         {`
-  //           body {
-  //             overflow: hidden;
-  //           }
-  //         `}
-  //       </style>
-  //             <video
-  //                 src='./videos/cadinov-intro.mp4'
-  //                 autoPlay
-  //                 loop
-  //                 style={{
-  //                 position: 'absolute',
-  //                 top: 0,
-  //                 left: 0,
-  //                 width: '1700px',
-  //                 height: '1260px',
-  //                 zIndex: -1,
-  //                 objectFit:'cover'
-  //                 }}
-  //             />
-              
-
-  //       <div
-  //         className='col-md-10 my-auto text-center'
-  //         style={{ borderRight: '8px solid white',  
-  //         background: 'linear-gradient(to bottom right, rgba(23, 85, 106, 1), rgba(204, 204, 204, 0.3))',
-  //         height: '420px'
-    
-  //     }}
-  //       >
-
-  // <style>
-  //         {`
-  //           .pulse-button {
-  //             animation: pulse 2s infinite;
-  //           }
-
-  //           @keyframes pulse {
-  //             0% {
-  //               transform: scale(1);
-  //             }
-  //             50% {
-  //               transform: scale(1.05);
-  //             }
-  //             100% {
-  //               transform: scale(1);
-  //             }
-  //           }
-  //         `}
-  //       </style>
-    
-  //         <img src='./images/cadinov_logo.png' className='pulse-image'
-  //         alt='Cadinov Logo' data-aos='zoom-in'
-  //         style={{width : '300px', height : '300px'}} />
-  //         <h1 data-aos='zoom-out' style={{ color: 'white' }}>
-  //           "Promoting the development of national and international tourism"
-  //         </h1>
-  //         <button className='btn btn-primary pulse-button' onClick={this.state.handleClick}>
-  //           Begin Your Adventure
-  //         </button>
-  //       </div>
-  //     </div>
-  //   );
-  }
+  )
 }
 
 export default LandingScreen
